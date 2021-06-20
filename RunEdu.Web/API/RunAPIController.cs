29d2b48ad7105d638605de570500d3868f3269c5
志云 申 <296932795@qq.com>
2021-06-20 19:03:04 +0800

    1

--- RunEdu.Web/API/RunAPIController.cs
+++ RunEdu.Web/API/RunAPIController.cs
@@ -30,7 +30,7 @@ namespace Edu.Web.API
         [HttpGet]
         public IHttpActionResult GetSocreRank(string WXUserID)
         {
-            var run = unitOfWork.DScoreRank.Get(p=>p.WXUserID==WXUserID);
+            var run = unitOfWork.DScoreRank.Get(p => p.WXUserID == WXUserID);
 
 
             return Json(new { R = true, Data = run });
@@ -43,8 +43,8 @@ namespace Edu.Web.API
             emphasis em = new emphasis();
             backgroundStyle bd = new backgroundStyle();
 
-         
-           HuoYueModel huating = new HuoYueModel();
+
+            HuoYueModel huating = new HuoYueModel();
             huating.name = "华庭队";
             huating.type = "bar";
             huating.label = "labelOption";
@@ -60,9 +60,9 @@ namespace Edu.Web.API
             lanting.name = "兰庭队";
             lanting.type = "bar";
             lanting.label = "labelOption";
-          
+
             lanting.emphasis = em;
-         
+
 
             HuoYueModel fengge = new HuoYueModel();
             fengge.name = "兰庭队";
@@ -75,12 +75,12 @@ namespace Edu.Web.API
             mingjun.name = "兰庭队";
             mingjun.type = "bar";
             mingjun.label = "labelOption";
-         
+
             mingjun.emphasis = em;
 
             HuoDate huodate = new HuoDate();
             huodate.type = "category";
-            axisTick ast= new axisTick();
+            axisTick ast = new axisTick();
             ast.show = "false";
 
 
@@ -128,9 +128,9 @@ LEFT JOIN (SELECT COUNT(*) as m,DATE_FORMAT(af5.CreateDate,'%m') as gptime from 
                 huodate.data = data.Select(p => p.m).ToList();
             }
 
-          
 
-            return Json(new { R = true, HT=huating,LT=lanting,FG=fengge,MJ=mingjun,DateList= huodate });
+
+            return Json(new { R = true, HT = huating, LT = lanting, FG = fengge, MJ = mingjun, DateList = huodate });
         }
 
 
@@ -148,8 +148,10 @@ LEFT JOIN (SELECT COUNT(*) as m,DATE_FORMAT(af5.CreateDate,'%m') as gptime from 
 
                 List<Points> runModel = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Points>>(postContent);
 
-                Edu.Tools.LogHelper.Info("跑步数据列表："+postContent);
+                Edu.Tools.LogHelper.Info("跑步数据列表：" + postContent);
                 double Dis = 0;
+
+                List<CardPoints> cardPoints = CardPointHelper.GetPoints();
 
                 if (runModel != null && runModel.Count() > 1)
                 {
@@ -159,19 +161,25 @@ LEFT JOIN (SELECT COUNT(*) as m,DATE_FORMAT(af5.CreateDate,'%m') as gptime from 
 
                         double d = RunHelper.GetDistance(runModel[i].latitude, runModel[i].longitude, runModel[i + 1].latitude, runModel[i + 1].longitude);
 
-                      
-                            List<CardPoints> cardPoints = CardPointHelper.GetPoints();
 
-                            foreach (var item in cardPoints)
+
+
+                        foreach (var item in cardPoints)
+                        {
+                            double d1 = RunHelper.GetDistance(runModel[i].latitude, runModel[i].longitude, item.latitude, item.longitude);
+
+                            if (d1 < 60)
                             {
-                                double d1 = RunHelper.GetDistance(runModel[i].latitude, runModel[i].longitude, item.latitude, item.longitude);
 
-                                if (d1 < 60)
-                                {
-                                    PlayScore = PlayScore + 10;
-                                }
+
+
+                                PlayScore = PlayScore + 10;
+
+                                cardPoints.Remove(item);
                             }
-                       
+
+                        }
+
 
 
                         long FTime = runModel[i].pointTimestamp;
@@ -240,18 +248,18 @@ LEFT JOIN (SELECT COUNT(*) as m,DATE_FORMAT(af5.CreateDate,'%m') as gptime from 
                     for (int i = 0; i < runModel.Count() - 1; i++)
                     {
 
-           
+
                         double d = RunHelper.GetDistance(runModel[i].latitude, runModel[i].longitude, runModel[i + 1].latitude, runModel[i + 1].longitude);
 
-                      
+
                         long FTime = runModel[i].pointTimestamp;
-                        long STime = runModel[i+1].pointTimestamp;
+                        long STime = runModel[i + 1].pointTimestamp;
 
                         double cha = (STime - FTime) / 1000;
 
-                        double speed = (d  / cha);
+                        double speed = (d / cha);
 
-                     
+
 
                         if (speed < 0.5 || speed > 7)
                         {
@@ -264,7 +272,7 @@ LEFT JOIN (SELECT COUNT(*) as m,DATE_FORMAT(af5.CreateDate,'%m') as gptime from 
 
 
                     }
-                    return Json(new { R = true, Data = (Dis/1000).ToString("0.000") });
+                    return Json(new { R = true, Data = (Dis / 1000).ToString("0.000") });
                 }
                 else
                 {
@@ -293,9 +301,9 @@ LEFT JOIN (SELECT COUNT(*) as m,DATE_FORMAT(af5.CreateDate,'%m') as gptime from 
             try
             {
                 string sql = @"SELECT @rownum:= @rownum + 1 as RankID,SUM(Totalkm) as Totalkm,A.WXUserID,B.NickName,b.HeadPhoto,B.Sex,C.TeamName,B.TeamID from(select @rownum:= 0) d,running as A ,userinfo as B,team as C WHERE  DATE_SUB(CURDATE(), INTERVAL 1 DAY) <= date(A.CreateDate) AND  A.WXUserID=B.WxID AND A.TeamID=C.ID  GROUP BY WXUserID ";
-             
-               var data = this.unitOfWork.context.Database.SqlQuery<RankModel>(sql, new object[0]);
-               var personData = data.Where(p=>p.WXUserID==WXUserID);
+
+                var data = this.unitOfWork.context.Database.SqlQuery<RankModel>(sql, new object[0]);
+                var personData = data.Where(p => p.WXUserID == WXUserID);
                 result = base.Json(new
                 {
                     R = true,
@@ -326,9 +334,9 @@ LEFT JOIN (SELECT COUNT(*) as m,DATE_FORMAT(af5.CreateDate,'%m') as gptime from 
                 {
                     "SELECT @rownum:= @rownum + 1 as RankID,  E.Totalkm,E.WXUserID,E.NickName,E.HeadPhoto,E.Sex,E.TeamName,E.TeamID from (SELECT  SUM(Totalkm) as Totalkm,A.WXUserID,B.NickName,B.HeadPhoto,B.Sex,C.TeamName,C.ID  as TeamID from running as A ,userinfo as B,team as C  WHERE A.TeamID=B.TeamID AND A.WXUserID=B.WxID AND A.TeamID = C.ID  AND A.TeamID="+TeamID.ToString()+"  AND A.TeamID=c.ID  AND  DATE_SUB(CURDATE(), INTERVAL "+DayCount.ToString()+" DAY) <= date(A.CreateDate)  GROUP BY A.WXUserID  ORDER BY Totalkm DESC) E, (select @rownum:= 0) d"
                 });
-              
+
                 List<TeamRunModel> data = this.unitOfWork.context.Database.SqlQuery<TeamRunModel>(sql, new object[0]).ToList<TeamRunModel>();
-                List<TeamRunModel> personData = data.Where(p=>p.WXUserID==WXUserID).ToList();
+                List<TeamRunModel> personData = data.Where(p => p.WXUserID == WXUserID).ToList();
                 result = base.Json(new
                 {
                     R = true,
@@ -371,11 +379,11 @@ LEFT JOIN (SELECT COUNT(*) as m,DATE_FORMAT(af5.CreateDate,'%m') as gptime from 
 
         public IHttpActionResult GetTodayRunList(string WxUserID)
         {
-            
-           var runList = unitOfWork.DRunning.Get(p => p.WXUserID == WxUserID).OrderByDescending(p=>p.ID).Take(30);
 
-          
-           return Json(new { R = true, Data = runList });
+            var runList = unitOfWork.DRunning.Get(p => p.WXUserID == WxUserID).OrderByDescending(p => p.ID).Take(30);
+
+
+            return Json(new { R = true, Data = runList });
 
         }
 
@@ -395,11 +403,11 @@ LEFT JOIN (SELECT COUNT(*) as m,DATE_FORMAT(af5.CreateDate,'%m') as gptime from 
 
         public IHttpActionResult GetPersonTotalRUn(string WxUserID)
         {
-           
 
 
-      
-            var runList = unitOfWork.DRunning.Get(p =>p.WXUserID == WxUserID);
+
+
+            var runList = unitOfWork.DRunning.Get(p => p.WXUserID == WxUserID);
 
             double minSpeedA = 100;
             double minSpeedB = 100;
@@ -416,7 +424,7 @@ LEFT JOIN (SELECT COUNT(*) as m,DATE_FORMAT(af5.CreateDate,'%m') as gptime from 
             int totalRunscore = 0;
             if (runList != null && runList.Count() > 0)
             {
-              
+
 
                 foreach (var item in runList)
                 {
@@ -438,7 +446,7 @@ LEFT JOIN (SELECT COUNT(*) as m,DATE_FORMAT(af5.CreateDate,'%m') as gptime from 
                     totalRunscore = totalRunscore + Convert.ToInt32(item.RunScore);
                 }
 
-            
+
             }
             if (minSpeedB == 100)
             {
@@ -448,7 +456,7 @@ LEFT JOIN (SELECT COUNT(*) as m,DATE_FORMAT(af5.CreateDate,'%m') as gptime from 
             {
                 tM.MinSpeed = minSpeedB.ToString();
             }
-         
+
             tM.TotalKM = TotalKm;
             tM.TotalHeat = Math.Round(totalHeat, 1);
             tM.RunDate = DateTime.Now.ToString("yyyy.MM.dd");
@@ -508,7 +516,7 @@ LEFT JOIN (SELECT COUNT(*) as m,DATE_FORMAT(af5.CreateDate,'%m') as gptime from 
 
                     minSpeedA = Convert.ToDouble(item.Speed);
 
-                    if (minSpeedB > minSpeedA&&minSpeedA!=0)
+                    if (minSpeedB > minSpeedA && minSpeedA != 0)
                     {
                         minSpeedB = minSpeedA;
                     }
@@ -563,62 +571,62 @@ LEFT JOIN (SELECT COUNT(*) as m,DATE_FORMAT(af5.CreateDate,'%m') as gptime from 
 
                 Running runModel = JsonConvert.DeserializeObject<Running>(value);
 
-                
+
                 UserInfo userInfo = this.unitOfWork.DUserInfo.Get((UserInfo p) => p.WxID == runModel.WXUserID, null, "").FirstOrDefault<UserInfo>();
                 bool flag = userInfo != null && userInfo.Weight != null;
                 if (flag)
                 {
 
-                   
-                    int pointScore =Convert.ToInt32( runModel.PointScore);
-             
-                    int TodayPointCount =Convert.ToInt32( unitOfWork.DRunning.Get(p => p.CreateDate>time1&&p.CreateDate<time2&&p.WXUserID==runModel.WXUserID).Sum(p=>p.PointScore));
-                    if (TodayPointCount > 0 )
-                    {
-                      
-                          
-                            if (TodayPointCount >= 40)
-                            {
-                                pointScore = 0;
-                            }
 
-                            if (pointScore + TodayPointCount > 100 && TodayPointCount < 40)
-                            {
-                                pointScore = 40 - TodayPointCount;
-                            }
-                       
-                       
+                    int pointScore = Convert.ToInt32(runModel.PointScore);
+
+                    int TodayPointCount = Convert.ToInt32(unitOfWork.DRunning.Get(p => p.CreateDate > time1 && p.CreateDate < time2 && p.WXUserID == runModel.WXUserID).Sum(p => p.PointScore));
+                    if (TodayPointCount > 0)
+                    {
+
+
+                        if (TodayPointCount >= 40)
+                        {
+                            pointScore = 0;
+                        }
+
+                        if (pointScore + TodayPointCount > 100 && TodayPointCount < 40)
+                        {
+                            pointScore = 40 - TodayPointCount;
+                        }
+
+
                     }
 
 
-                 
+
                     double num = Convert.ToDouble(userInfo.Weight);
                     double num2 = Convert.ToDouble(runModel.Totalkm);
                     int RunScore = 0 + Convert.ToInt32(10 * num2);
 
-                  
-                    int TodayRunCount = Convert.ToInt32(unitOfWork.DRunning.Get(p => p.CreateDate > time1 && p.CreateDate < time2 && p.WXUserID == runModel.WXUserID).Sum(p => p.RunScore));
-               
 
-                    if (TodayRunCount>0)
+                    int TodayRunCount = Convert.ToInt32(unitOfWork.DRunning.Get(p => p.CreateDate > time1 && p.CreateDate < time2 && p.WXUserID == runModel.WXUserID).Sum(p => p.RunScore));
+
+
+                    if (TodayRunCount > 0)
                     {
-                       
-                         
-                            if (TodayRunCount >= 100)
-                            {
-                                RunScore = 0;
-                            }
-                            if (RunScore + TodayRunCount > 100 && TodayRunCount < 100)
-                            {
-                                RunScore = 100 - TodayRunCount;
-                            }
-                       
+
+
+                        if (TodayRunCount >= 100)
+                        {
+                            RunScore = 0;
+                        }
+                        if (RunScore + TodayRunCount > 100 && TodayRunCount < 100)
+                        {
+                            RunScore = 100 - TodayRunCount;
+                        }
+
                     }
 
 
                     runModel.RunScore = RunScore;
                     runModel.PointScore = pointScore;
-                  
+
                     runModel.TotalScore = RunScore + pointScore;
                     int hour = Convert.ToDateTime(runModel.TotalTime).Hour;
                     int minute = Convert.ToDateTime(runModel.TotalTime).Minute;
@@ -633,12 +641,12 @@ LEFT JOIN (SELECT COUNT(*) as m,DATE_FORMAT(af5.CreateDate,'%m') as gptime from 
                         scoreRank.RunTypeID = 1;
                         scoreRank.RunTypeName = "跑步获取";
                         scoreRank.Score = RunScore;
-                        scoreRank.CreateDate =Convert.ToDateTime( DateTime.Now.ToShortDateString());
+                        scoreRank.CreateDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
                         this.unitOfWork.DScoreRank.Insert(scoreRank);
                         this.unitOfWork.Save();
                     }
 
-                  
+
                     if (pointScore > 0)
                     {
                         ScoreRank scoreRank = new ScoreRank();
@@ -650,7 +658,7 @@ LEFT JOIN (SELECT COUNT(*) as m,DATE_FORMAT(af5.CreateDate,'%m') as gptime from 
                         this.unitOfWork.DScoreRank.Insert(scoreRank);
                         this.unitOfWork.Save();
                     }
-                  
+
                     int totalScore = pointScore + RunScore;
                     if (totalScore > 0)
                     {
@@ -672,10 +680,10 @@ LEFT JOIN (SELECT COUNT(*) as m,DATE_FORMAT(af5.CreateDate,'%m') as gptime from 
                             this.unitOfWork.DScore.Insert(_score);
                             this.unitOfWork.Save();
                         }
-                       
+
                     }
 
-                  
+
 
 
                 }
@@ -683,7 +691,7 @@ LEFT JOIN (SELECT COUNT(*) as m,DATE_FORMAT(af5.CreateDate,'%m') as gptime from 
                 this.unitOfWork.DRunning.Insert(runModel);
                 this.unitOfWork.Save();
 
-              
+
 
 
 
@@ -691,7 +699,7 @@ LEFT JOIN (SELECT COUNT(*) as m,DATE_FORMAT(af5.CreateDate,'%m') as gptime from 
                 {
                     R = true,
                     M = "跑步信息添加成功！",
-                    Data=runModel
+                    Data = runModel
                 });
             }
             catch (Exception ex)
