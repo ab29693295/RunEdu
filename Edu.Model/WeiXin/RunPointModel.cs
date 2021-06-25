99b5c358b1fa93a1b43917f09e2fd6806bffa21f
志云 申 <296932795@qq.com>
2021-06-25 18:13:27 +0800

    1

--- Edu.Model/WeiXin/RunPointModel.cs
+++ Edu.Model/WeiXin/RunPointModel.cs
@@ -60,11 +60,8 @@ namespace Edu.Model.WeiXin
             cdPoints.longitude = 117.7488784790039;
             cdPoints.status = 0;
 
-            cdPoints.latitude =  39.11765657043457;
-            cdPoints.longitude = 117.7488784790039;
-
-            cdPoints1.latitude =   39.11702346801758;
-            cdPoints1.longitude = 117.7497329711914;
+         
+  
 
 
             cdPoints1.latitude = 39.11702346801758;
@@ -76,12 +73,9 @@ namespace Edu.Model.WeiXin
             cdPoints2.longitude = 117.74925231933594;
             cdPoints2.status = 0;
 
-            cdPoints2.latitude =  39.117401123046875;
-            cdPoints2.longitude = 117.74925231933594;
+        
 
-            cdPoints3.latitude = 39.1178894004296875;
-            cdPoints3.longitude = 117.74901580810547;
->>>>>>> d24706e1d5e513c95dd96128a8fc871e33ecb3d2
+      
 
             cdPoints3.latitude = 39.1178894004296875;
             cdPoints3.longitude = 117.74901580810547;
